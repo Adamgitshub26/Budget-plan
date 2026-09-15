f9e22cfa249ca58826c5b133619199c6f2ae1db8
Adamgitshub26 <adamta101@gmail.com>
2026-09-15 15:00:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump CACHE version whenever you change app files, so devices fetch the new copy.
-const CACHE = "budgets-v1.8";
+const CACHE = "budgets-v2.0";
 const ASSETS = [
   "./",
   "./index.html",
